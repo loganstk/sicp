9830750422a738c0823dc47531fb6805c355316e
Andrii Lohvynchuk <logan.zhi@gmail.com>
2022-12-09 00:41:18 +0200

    Add more excercises from chapter 1

--- chapter1.scm
+++ chapter1.scm
@@ -11,8 +11,9 @@
 
 (define circumference (* 2 pi radius))
 
-(define (square x)
-  (* x x))
+(define (square x) (* x x))
+
+(define (cube x) (* x x x))
 
 ; (square 2)
 
@@ -217,3 +218,38 @@
                           q
                           (- count 1)))))
   (fib-iter 1 0 0 1 n))
+
+; Greates common divisors
+(define (gcd a b)
+  (if (zero? b)
+      a
+      (gcd b (remainder a b))))
+
+
+(define (sum term a next b)
+  (if (> a b) 0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(define (identity x) x)
+
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(define (integral f a b dx)
+  (define (add-dx x)
+    (+ x dx))
+  (* (sum f (+ a (/ dx 2)) add-dx b)
+     dx))
