26bd68225812a59f7ef5d0b8b4c29ad2cf260941
Andrii Lohvynchuk <logan.zhi@gmail.com>
2023-01-06 01:40:27 +0200

    Add picture language definitions

---
+++ chapter2/9-picture-language.scm
@@ -0,0 +1,61 @@
+#lang sicp
+
+(#%require sicp-pict)
+
+; 2.2.4 Picture Language
+
+; (c) https://stackoverflow.com/a/41507786/5660182
+(define wave
+  (segments->painter
+   (list
+    (make-segment (make-vect 0.20 0.00) (make-vect 0.35 0.50))
+    (make-segment (make-vect 0.35 0.50) (make-vect 0.30 0.60))
+    (make-segment (make-vect 0.30 0.60) (make-vect 0.15 0.45))
+    (make-segment (make-vect 0.15 0.45) (make-vect 0.00 0.60))
+    (make-segment (make-vect 0.00 0.80) (make-vect 0.15 0.65))
+    (make-segment (make-vect 0.15 0.65) (make-vect 0.30 0.70))
+    (make-segment (make-vect 0.30 0.70) (make-vect 0.40 0.70))
+    (make-segment (make-vect 0.40 0.70) (make-vect 0.35 0.85))
+    (make-segment (make-vect 0.35 0.85) (make-vect 0.40 1.00))
+    (make-segment (make-vect 0.60 1.00) (make-vect 0.65 0.85))
+    (make-segment (make-vect 0.65 0.85) (make-vect 0.60 0.70))
+    (make-segment (make-vect 0.60 0.70) (make-vect 0.75 0.70))
+    (make-segment (make-vect 0.75 0.70) (make-vect 1.00 0.40))
+    (make-segment (make-vect 1.00 0.20) (make-vect 0.60 0.48))
+    (make-segment (make-vect 0.60 0.48) (make-vect 0.80 0.00))
+    (make-segment (make-vect 0.40 0.00) (make-vect 0.50 0.30))
+    (make-segment (make-vect 0.50 0.30) (make-vect 0.60 0.00)))))
+
+(define wave2 (beside wave (flip-vert wave)))
+; (define wave4 (below wave2 wave2))
+
+(define (flipped-pairs painter)
+  (let ((painter2 (beside painter (flip-vert painter))))
+    (below painter2 painter2)))
+
+(define wave4 (flipped-pairs wave))
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+; Should I define this myself? OK...
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter (beside smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let* ((up (up-split painter (- n 1)))
+             (right (right-split painter (- n 1)))
+             (top-left (beside up up))
+             (bottom-right (below right right))
+             (corner (corner-split painter (- n 1))))
+        (beside (below painter top-left)
+                (below bottom-right corner)))))
+
